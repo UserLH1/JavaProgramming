7350a15af2e43989e851dd7b63562650b5c7c4f1
UserLH1 <114158851+UserLH1@users.noreply.github.com>
2023-11-06 20:56:30 +0200

    commitul de acasa

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
-public class Main
-{
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Film> listaDeFilme = new ArrayList<Film>();
+
+        listaDeFilme.add(new Film("Titlu Film 1", "Regizor 1", 1000000));
+        listaDeFilme.add(new Documentar("Titlu Documentar", "Regizor Documentar", 500000, 5, "Descriere subiect documentar"));
+        List<Actor> listaDeActori = new ArrayList<Actor>();
+        listaDeActori.add(new Actor("Actor 1", "abilitati dans", "abilitati cantat"));
+        listaDeActori.add(new Actor("Actor 2", "abilitati dans", "abilitati cantat"));
+        listaDeFilme.add(new Muzical("Titlu Muzical", "Regizor Muzical", 1500000, listaDeActori));
+
+        for (Film film : listaDeFilme) {
+            System.out.println(film);
+        }
     }
 }
--- Muzical.java
+++ Muzical.java
@@ -2,4 +2,29 @@ import java.util.List;
 public class Muzical extends Film
 {
     private List<Actor> lista;
+
+    public Muzical(String titlu, String regizor, double buget, List<Actor> lista) {
+        super(titlu, regizor, buget);
+        this.lista = lista;
+    }
+
+    public Muzical(List<Actor> lista) {
+        this.lista = lista;
+    }
+
+    @Override
+    public String toString() {
+        return "Muzical{" +
+                "lista=" + lista +
+                '}';
+    }
+
+    public List<Actor> getLista() {
+        return lista;
+    }
+
+    public void setLista(List<Actor> lista) {
+        this.lista = lista;
+    }
+
 }
